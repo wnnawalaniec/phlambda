38c050dd8c5b2d00c5536a31fee9ab7ce51961ea
Wojciech Nawalaniec <wnnawalaniec@gmail.com>
2022-01-12 00:05:52 +0100

    Add either()

--- src/logic.php
+++ src/logic.php
@@ -47,3 +47,27 @@ function both(callable...$v): callable
 }
 
 const both = '\Wojciech\Phlambda\both';
+
+/**
+ * Function calling two provided functions and returning the `||` of the results.
+ *
+ * This functions accepts two predicates and then return function accepting one parameter and calling both predicates
+ * with that parameter. Then `||` of both results is returned.
+ * Keep in mind that both given functions will be called.
+ *
+ * Example:
+ * <blockquote><pre>either(above(5), below(3))(1) // it will return true</pre></blockquote>
+ * <blockquote><pre>filter(either(above(18), below(30), [11, 19, 26, 40]); // it will return [11, 19, 26, 40]</pre></blockquote>
+ *
+ * @see _and()
+ * @param callable(mixed): bool $fn1
+ * @param callable(mixed): bool $fn2
+ * @return callable
+ */
+#[ShouldNotBeImplementedInWrapper]
+function either(callable...$v): callable
+{
+    return curry2(fn (callable $fn1, callable $fn2): callable => fn (mixed $x) => $fn1($x) || $fn2($x))(...$v);
+}
+
+const either = '\Wojciech\Phlambda\either';
--- tests/LogicTest.php
+++ tests/LogicTest.php
@@ -5,9 +5,16 @@ namespace Tests\Wojciech\Phlambda;
 
 use function Wojciech\Phlambda\_and;
 use function Wojciech\Phlambda\both;
+use function Wojciech\Phlambda\either;
 
 class LogicTest extends BaseTest
 {
+    public function testAnd(): void
+    {
+        $this->assertSame(true, _and(true, true));
+        $this->assertSame(false, _and(true, false));
+    }
+
     public function testBoth(): void
     {
         $isEven = fn (int$a) => $a % 2 === 0;
@@ -16,9 +23,10 @@ class LogicTest extends BaseTest
         $this->assertSame(false, both('is_integer', $isEven)(1.0));
     }
 
-    public function testAnd(): void
+    public function testEither(): void
     {
-        $this->assertSame(true, _and(true, true));
-        $this->assertSame(false, _and(true, false));
+        $this->assertSame(true, either('is_integer', 'is_string')(2));
+        $this->assertSame(true, either('is_integer', 'is_string')('2'));
+        $this->assertSame(false, either('is_integer', 'is_string')(1.0));
     }
 }
