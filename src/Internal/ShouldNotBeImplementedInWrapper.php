a01c7d5deb1e071c45ec67ccfd1ef2f72b248dec
Wojciech Nawalaniec <wnnawalaniec@gmail.com>
2022-01-11 23:12:12 +0100

    Add some comment

--- src/Internal/ShouldNotBeImplementedInWrapper.php
+++ src/Internal/ShouldNotBeImplementedInWrapper.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace Wojciech\Phlambda\Internal;
 
+use Wojciech\Phlambda\Wrapper;
+
 /**
+ * Attribute to mark methods that are not supposed to be implemented by Wrapper.
+ *
+ * @see Wrapper
  * @codeCoverageIgnore
  */
 #[\Attribute]
